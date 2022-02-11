bd3a1a51e97a01763b7afafb3491c0c79d6ca9ae
kevin <kevin.vandenbreemen@gmail.com>
2022-02-11 15:26:23 -0800

    Start work on UI for displaying the data

---
+++ app/src/main/java/com/vandenbreemen/android/diagnostics/view/DiagnosticFragment.kt
@@ -0,0 +1,13 @@
+package com.vandenbreemen.android.diagnostics.view
+
+import androidx.fragment.app.Fragment
+import com.vandenbreemen.common.diagnostics.DiagnosticsData
+
+/**
+ * Display diagnostic info
+ */
+class DiagnosticFragment(private val diagnosticsData: DiagnosticsData): Fragment() {
+
+
+
+}
